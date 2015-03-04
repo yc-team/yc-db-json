efb4502f87e380d954c0ab791e6998f14d6032ab
zhangyaochun <zhangyaochun@wandoujia.com>
2015-03-04 12:48:00 +0800

    [command line] commit auto by @wd-deploy on 2015-03-04 12:48:00

--- test/test.js
+++ test/test.js
@@ -15,4 +15,8 @@ console.log(_.insert(db.user, {name: 'insert by auto', id: 2}));
 //{ name: 'insert by auto', id: 1 }
 console.log(_.insert(db.user, {name: 'insert by auto', id: 1}));
 
+//{ name: 'update by auto', id: 1, age: 20 }
+console.log(_.update(db.user, 1, {name: 'update by auto', age: 20}));
+
+
 _.save(db);
